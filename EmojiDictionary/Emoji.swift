15c4f19fdf614e4758d45cfa4032e27fa9d1e93c
Tomiwa Babalola <tbabalola004@gmail.com>
2022-02-10 10:59:38 -0500

    hbkjbkh

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,14 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Tomiwa Babalola on 2/10/22.
+//
+
+import Foundation
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
